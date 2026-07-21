e919f61d6e9cc3bc8fde09e820fa549e0f75a2d1
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-22 00:58:22 +0530

    handling double and sinlg click actions

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import cv2
 import mediapipe as mp
 import pyautogui
+import time
+import math
 
 mp_hands = mp.solutions.hands
 mp_drawing= mp.solutions.drawing_utils
 hands=mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7)
 
+cap = cv2.VideoCapture(0)
+
+#gesture time control
+click_start_time=None
+click_times=[]
+click_cooldown=0.5
+scroll_mode=False
+freeze_cursor=False
+
 screen_w,screen_h = pyautogui.size()
 print("\n hand mouse control.")
-prev_screen_x=0, prev_screen_y=0,0
-cap = cv2.VideoCapture(0)
+prev_screen_x, prev_screen_y = 0,0
 
 if not cap.isOpened():
     print("Cannnot Open Camera")
@@ -29,14 +39,38 @@ while True:
             mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
         #get finger tips
-        thumb_tip=hand_landmarks.landmarks[4]
-        index_tip=hand_landmarks.landmarks[8]
-        middle_tip=hand_landmarks.landmarks[12]
-        ring_tip=hand_landmarks.landmarks[16]
+        thumb_tip=hand_landmarks.landmark[4]
+        index_tip=hand_landmarks.landmark[8]
+        middle_tip=hand_landmarks.landmark[12]
+        ring_tip=hand_landmarks.landmark[16]
 
         fingers=[
-            1 if hand_landmarks.landmark[tip].y < hand_landmarks.landmark[tip-2].y else 0
+            1 if hand_landmarks.landmark[tip].y < hand_landmarks.landmark[tip-2].y else 0 
+            for tip in [8, 12, 16, 20]
         ]
+
+        #distance between thumb 
+        dist=math.hypot(thumb_tip.x-index_tip.x, thumb_tip.y-index_tip.y)
+        if dist<0.06:
+            if not freeze_cursor:
+                freeze_cursor=True
+                click_times.append(time.time())
+
+                # double click check
+                if len(click_times)>=2 and click_times[-1]-click_times[-2]<0.4:
+                    pyautogui.doubleClick()
+                    cv2.putText(frame, "Double Click", (10,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,255), 2)
+                    click_times=[]
+                # single click
+                else:
+                    pyautogui.click()
+                    cv2.putText(frame, "Single Click", (10,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,0), 2)
+
+        
+
+            
+
+            
 
 
     cv2.imshow("live video", frame) 
