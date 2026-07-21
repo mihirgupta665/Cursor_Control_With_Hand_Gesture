e6831779ec7db90829340e350e21297d171fb2b7
Mihir Gupta <mihirgupta665@gmail.com>
2026-07-21 15:28:48 +0530

    add the feature in app to show hand larnmarks and all the required points in camera while capturing the video also added the gitignore

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import cv2
+import mediapipe as mp
+mp_hands = mp.solutions.hands
+mp_drawing= mp.solutions.drawing_utils
+hands=mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7)
+
 cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
@@ -12,6 +17,12 @@ while True:
         break
 
     frame=cv2.flip(frame, 1)
+    rgb=cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    result = hands.process(rgb)
+    if result.multi_hand_landmarks:
+        for hand_landmarks in result.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
     cv2.imshow("live video", frame) 
 
     if cv2.waitKey(1)==ord("q"):
